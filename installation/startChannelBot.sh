e9de92813a0ffaa80a6c22d656e21b3226aeadb0
jo <28813094+J03D03@users.noreply.github.com>
2020-12-17 14:15:58 +0100

    Minor changes

--- installation/startChannelBot.sh
+++ installation/startChannelBot.sh
@@ -3,7 +3,7 @@
 # pull the bot from dockerhub
 docker pull j03d03/teamspeak-channel-bot:latest
 # stop and remove container
-docker stop ChannelBot
-docker rm ChannelBot
+# docker stop ChannelBot
+# docker rm ChannelBot
 # start container
 docker run --name ChannelBot --restart always -d --env-file ./env.list --network="host" j03d03/teamspeak-channel-bot:latest
--- installation/startLegacyBot.sh
+++ installation/startLegacyBot.sh
@@ -3,7 +3,7 @@
 # pull the bot from dockerhub
 docker pull j03d03/teamspeak-legacy-bot:latest
 # stop and remove container
-docker stop LegacyBot
-docker rm LegacyBot
+# docker stop LegacyBot
+# docker rm LegacyBot
 # start container
 docker run --name LegacyBot --restart always -d --env-file ./env.list --network="host" j03d03/teamspeak-legacy-bot:latest
